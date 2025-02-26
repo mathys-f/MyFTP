9db8c09dd86893e897d058e38dd0420823b22cce
mathys-f <mathys.fillion@epitech.eu>
2025-02-26 22:46:29 +0100

    ✨ Add create_poll function to initialize poll file descriptors

--- src/loop.c
+++ src/loop.c
@@ -11,6 +11,21 @@
 
 #include "loop.h"
 
+static struct pollfd *create_poll(int server_fd)
+{
+    struct pollfd *fds = malloc(sizeof(struct pollfd) * 11);
+
+    if (fds == NULL)
+        exit(84);
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;
+    for (int i = 1; i < 11; i++) {
+        fds[i].fd = -1;
+        fds[i].events = POLLIN;
+    }
+    return fds;
+}
+
 int run_server(int port, char *path)
 {
     int server_fd = create_server(port);
