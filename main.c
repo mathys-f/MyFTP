4ab1bdb3f82de1a81c1133f9cf208110da7ab1f5
mathys-f <mathys.fillion@epitech.eu>
2025-02-26 22:34:37 +0100

    ✨ Add argument handling for helper and port

--- main.c
+++ main.c
@@ -5,7 +5,19 @@
 ** main.c
 */
 
+#include <string.h>
+#include <stdlib.h>
+
 int main(int ac, char **av)
 {
+    int port;
+
+    if (ac == 2 && strcmp(av[1], "-help") == 0)
+        return 0;
+    if (ac != 3)
+        return 84;
+    port = atoi(av[1]);
+    if (port == 0)
+        return 84;
     return 0;
 }
