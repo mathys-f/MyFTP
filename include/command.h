de69c4bc70d12d6adffe6a3fb05dcab7119728d3
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 06:48:46 +0100

    ✨ Add command.h header file for client command handling

---
+++ include/command.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** command.c
+** File description:
+** command.c
+*/
+
+#ifndef COMMAND_H
+    #define COMMAND_H
+
+    #include "client.h"
+
+void handle_client_command(client_t *client);
+
+#endif /* !COMMAND_H */
