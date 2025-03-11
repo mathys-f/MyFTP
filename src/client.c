34534a42a9191d3ba59b47895cfd6f9ee03e3c38
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 03:11:50 +0100

    ✨ Implement add_client function to manage client connections in the FTP server

--- src/client.c
+++ src/client.c
@@ -12,6 +12,17 @@
 #include "client.h"
 #include "loop.h"
 
+void add_client(my_ftp_t *my_ftp, int fd)
+{
+    my_ftp->fds = realloc(my_ftp->fds, sizeof(struct pollfd) *
+        (my_ftp->nb_fds + 1));
+    if (my_ftp->fds == NULL)
+        exit(84);
+    my_ftp->fds[my_ftp->nb_fds].fd = fd;
+    my_ftp->fds[my_ftp->nb_fds].events = POLLIN;
+    my_ftp->nb_fds++;
+}
+
 struct pollfd *create_poll(int server_fd)
 {
     struct pollfd *fds = malloc(sizeof(struct pollfd));
