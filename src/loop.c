9f72803c461d00545a75d493d6366cd5b37bee5a
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 03:09:40 +0100

    ✨ Refactor server loop to use my_ftp_t structure and manage server state

--- src/loop.c
+++ src/loop.c
@@ -12,40 +12,23 @@
 #include "loop.h"
 #include "server.h"
 
-static struct pollfd *create_poll(int server_fd)
+static void loop(my_ftp_t *my_ftp)
 {
-    struct pollfd *fds = malloc(sizeof(struct pollfd) * 11);
-
-    if (fds == NULL)
-        exit(84);
-    fds[0].fd = server_fd;
-    fds[0].events = POLLIN;
-    for (int i = 1; i < 11; i++) {
-        fds[i].fd = -1;
-        fds[i].events = POLLIN;
-    }
-    return fds;
-}
-
-static void destroy_poll(struct pollfd *fds)
-{
-    free(fds);
-}
-
-static void loop(int server_fd, char *path)
-{
-    (void)server_fd;
-    (void)path;
     while (1);
 }
 
 int run_server(int port, char *path)
 {
-    int server_fd = create_server(port);
-    struct pollfd *fds = create_poll(server_fd);
+    my_ftp_t *my_ftp = malloc(sizeof(my_ftp_t));
 
-    loop(server_fd, path);
-    destroy_poll(fds);
-    destroy_server(server_fd);
+    if (my_ftp == NULL)
+        exit(84);
+    my_ftp->port = port;
+    my_ftp->path = path;
+    my_ftp->server_fd = create_server(port);
+    my_ftp->fds = create_poll(my_ftp->server_fd);
+    my_ftp->nb_fds = 1;
+    loop(my_ftp);
+    down_server(my_ftp);
     return 0;
 }
