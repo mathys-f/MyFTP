01329e9ca5bb5bd8c738e4b90019444d1b37cbfd
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 06:48:20 +0100

    ✨ Define client_t structure to manage client state and credentials in client.h

--- include/client.h
+++ include/client.h
@@ -9,7 +9,17 @@
     #define CLIENT_H
 
     #include <poll.h>
+    #include <stdbool.h>
+
     #include "loop.h"
+
+typedef struct client_s {
+    int fd;
+    bool is_logged;
+    char *username;
+    char *password;
+    char *path;
+} client_t;
 
 void add_client(my_ftp_t *my_ftp, int fd);
 struct pollfd *create_poll(int server_fd);
