b17b7f6fc0fc880cc490f4462a6bfe7ea5abdc8d
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 08:40:23 +0100

    ✨ Implement user command handling in user.c

---
+++ src/commands/user.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** user.c
+** File description:
+** user.c
+*/
+
+#include <unistd.h>
+#include <string.h>
+
+#include "loop.h"
+#include "client.h"
+
+void user(my_ftp_t *my_ftp, client_t *client, char **args)
+{
+    (void)my_ftp;
+    if (client->is_logged == true) {
+        write(client->fd, "530 Already logged in.\r\n", 24);
+        return;
+    }
+    if (args[1] == NULL) {
+        write(client->fd, "501 Syntax error in parameters or arguments.\r\n", 45);
+        return;
+    }
+    client->username = strdup(args[1]);
+    write(client->fd, "331 User name okay, need password.\r\n", 34);
+}
