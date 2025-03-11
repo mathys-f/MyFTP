41fbfaaad74a5db23fcbfdbf859ab1544975d8d7
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 12:15:11 +0100

    ✨ Refactor command handling to support file descriptor and improve client command retrieval

--- src/command.c
+++ src/command.c
@@ -9,43 +9,58 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#
 
 #include "command.h"
 
 static const command_t commands[] = {
     {"QUIT", &quit},
     {"USER", &user},
+    {"PASS", &pass},
     {NULL, NULL}
 };
 
-static char *get_client_command(client_t *client)
+static char *get_client_command(int fd)
 {
-    char buffer[1024] = {0};
-    ssize_t nb_read = 0;
-    char *command = NULL;
+    char *command = malloc(sizeof(char) * 256);
+    int size = 0;
+    ssize_t readed = 0;
 
-    nb_read = read(client->fd, buffer, 1024);
-    if (nb_read == -1)
-        return NULL;
-    command = strdup(buffer);
+    if (command == NULL)
+        exit(84);
+    while (1) {
+        readed = read(fd, command + size, 256);
+        if (readed == -1)
+            exit(84);
+        if (readed == 0)
+            break;
+        size += readed;
+        if (size >= 2 && command[size - 2] == '\r' && command[size - 1] == '\n') {
+            command[size - 2] = '\0';
+            break;
+        }
+    }
     return command;
 }
 
-void handle_client_command(client_t *client)
+void handle_client_command(my_ftp_t *my_ftp, int fd)
 {
-    char *command = get_client_command(client);
-    char **args = my_str_to_word_array(command, " \t\n");
+    char *command = get_client_command(fd);
+    char **args = my_str_to_word_array(command, " \t");
+    client_t *current = my_ftp->clients;
 
     if (args == NULL)
         return;
-    for (int i = 0; commands[i].name; i++) {
+    while (current != NULL && current->fd != fd)
+        current = current->next;
+    for (int i = 0; commands[i].name != NULL; i++) {
         if (strcmp(commands[i].name, args[0]) == 0) {
-            commands[i].func(NULL, client, args);
+            commands[i].func(my_ftp, current, args);
             break;
         }
     }
     free(command);
-    for (int i = 0; args[i]; i++)
+    for (int i = 0; args[i] != NULL; i++)
         free(args[i]);
     free(args);
 }
