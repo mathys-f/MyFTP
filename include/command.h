b26dada71474dc34b7a994c7b6bb635135ff4e27
mathys-f <mathys.fillion@epitech.eu>
2025-03-11 08:00:18 +0100

    ✨ Add command_t struct and quit function declaration in command.h

--- include/command.h
+++ include/command.h
@@ -10,6 +10,13 @@
 
     #include "client.h"
 
+typedef struct command_s {
+    char *name;
+    void (*func)(my_ftp_t *my_ftp, client_t *client, char **args);
+} command_t;
+
+void quit(my_ftp_t *my_ftp, client_t *client, char **args);
+
 void handle_client_command(client_t *client);
 char **my_str_to_word_array(char *str, char *delimiters);
 
